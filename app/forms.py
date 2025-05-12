eb91db5ba19aafdb637e8a5756e8edc52790c107
HudsonWayne <waynewaynebenhura@gmail.com>
2025-05-12 11:26:46 +0200

    added EventForm

--- app/forms.py
+++ app/forms.py
@@ -1,3 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, DateTimeLocalField, SubmitField
 from wtforms.validators import DataRequired
+
+
+
+class EventForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    start = DateTimeLocalField('Start Time', format='%Y-%m-%dT%H:%M', validators=[DataRequired()])
+    end = DateTimeLocalField('End Time', format='%Y-%m-%dT%H:%M')
