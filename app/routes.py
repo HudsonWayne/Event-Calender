7899fa3efe9d4d6255079c3a7eeadf3fe2efab72
HudsonWayne <waynewaynebenhura@gmail.com>
2025-05-12 11:38:55 +0200

    added a blue print

--- app/routes.py
+++ app/routes.py
@@ -2,3 +2,6 @@ from flask import Blueprint, render_template, redirect, url_for, request, jsonif
 from .models import Event
 from .forms import EventForm
 from . import db
+
+
+main = Blueprint('main', __name__)
