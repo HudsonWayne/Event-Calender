568f0f21a31f285c4944e6fbb90080b79af9b894
HudsonWayne <waynewaynebenhura@gmail.com>
2025-05-12 11:23:55 +0200

    finished class event

--- app/models.py
+++ app/models.py
@@ -3,3 +3,8 @@ from datetime import datetime
 
 class Event (db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    start = db.Column(db.String, nullable=False)  # ISO format
+    end = db.Column(db.String, nullable=True)
+    description = db.Column(db.Text)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
