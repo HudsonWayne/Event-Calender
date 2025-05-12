14297ba1a2716072ebbdac1bff93ea233ce51efb
HudsonWayne <waynewaynebenhura@gmail.com>
2025-05-12 10:39:44 +0200

    defined my database and created my function to create app

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
